74c09e42fe7c7f03eadc056d9598d8d462388410
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-13 17:36:31 +0530

    UC-2 :- Found maximum of 3 float numbers.

--- src/com/find/maximum/generics/Find_Maximum.java
+++ src/com/find/maximum/generics/Find_Maximum.java
@@ -3,16 +3,31 @@ package com.find.maximum.generics;
 public class Find_Maximum {
 	// Method with parameters as Integer
 	public void integerMax(Integer N1 , Integer N2 , Integer N3) {
+		
+		// comparing numbers using compareTo method.
+	    if(N1.compareTo(N2) > 0 && N1.compareTo(N3) > 0) {
+	    	System.out.println("\nTC-1.1 :- " + N1 + " is maximum.");
+	    }
+	    else if(N2.compareTo(N1) > 0 && N2.compareTo(N3) > 0) {
+	        System.out.println("\nTC-1.2 :- " + N2 + " is maximum.");
+	    }
+	    else {
+	    	System.out.println("\nTC-1.3 :- " + N3 + " is maximum.");
+	    }
+	}
+	
+	// Method with parameters as Float
+	public void floatMax(Float N1 , Float N2 , Float N3) {
 
 		// comparing numbers using compareTo method.
 	    if(N1.compareTo(N2) > 0 && N1.compareTo(N3) > 0) {
-	    	System.out.println("\n" + N1 + " is maximum.");
+	    	System.out.println("\n\nTC-1.1 :- " + N1 + " is maximum.");
 	    }
 	    else if(N2.compareTo(N1) > 0 && N2.compareTo(N3) > 0) {
-	        System.out.println("\n" + N2 + " is maximum.");
+	        System.out.println("\nTC-1.2 :- " + N2 + " is maximum.");
 	    }
 	    else {
-	    	System.out.println("\n" + N3 + " is maximum.");
+	        System.out.println("\nTC-1.3 :- " + N3 + " is maximum.");
 	    }
 	}
 }
--- src/com/find/maximum/generics/Main_Find_Max.java
+++ src/com/find/maximum/generics/Main_Find_Max.java
@@ -8,9 +8,14 @@ public class Main_Find_Max {
 
 	    Find_Maximum object = new Find_Maximum();
 	    
-	    /** Test Cases **/
+	    /** Test Cases for integers. **/
 	    object.integerMax(45 , 38 , 24); //TC-1.1 :- max num at first position.
 	    object.integerMax(18, 28, 19);  //TC-1.2 :- max num at second position.
 	    object.integerMax(75, 52, 120); //TC-1.3 :- max num at third position.
+	
+	    /** Test cases for floats. **/
+	    object.floatMax(15.5f, 12.8f, 9.56f); //TC-1.1 :- max num at first position.
+	    object.floatMax(12.4f, 25.8f, 15.7f); //TC-1.2 :- max num at second position.
+	    object.floatMax(14.2f, 18.7f, 28.9f); //TC-1.3 :- max num at third position.
 	}
 }
