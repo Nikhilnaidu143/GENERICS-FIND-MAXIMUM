51da39739bc69dd2d8a623b7373aa41700376475
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-13 17:32:51 +0530

    START :- Displayed welcome message.

---
+++ src/com/find/maximum/generics/Find_Maximum.java
@@ -0,0 +1,8 @@
+package com.find.maximum.generics;
+
+public class Find_Maximum {
+	public static void main(String[] args) {
+		/** Displayed welcome message. **/
+		System.out.println("--------------FIND MAXIMUM USING GENERICS---------------");
+	}
+}
