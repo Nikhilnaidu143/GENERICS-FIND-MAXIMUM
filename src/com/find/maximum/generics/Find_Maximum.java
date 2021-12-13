37b22be21ed4866cc5de3261b18c066ed53905f8
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-13 17:34:45 +0530

    UC-1 :- Found maximum number of 3 integers.

--- src/com/find/maximum/generics/Find_Maximum.java
+++ src/com/find/maximum/generics/Find_Maximum.java
@@ -1,8 +1,18 @@
 package com.find.maximum.generics;
 
 public class Find_Maximum {
-	public static void main(String[] args) {
-		/** Displayed welcome message. **/
-		System.out.println("--------------FIND MAXIMUM USING GENERICS---------------");
+	// Method with parameters as Integer
+	public void integerMax(Integer N1 , Integer N2 , Integer N3) {
+
+		// comparing numbers using compareTo method.
+	    if(N1.compareTo(N2) > 0 && N1.compareTo(N3) > 0) {
+	    	System.out.println("\n" + N1 + " is maximum.");
+	    }
+	    else if(N2.compareTo(N1) > 0 && N2.compareTo(N3) > 0) {
+	        System.out.println("\n" + N2 + " is maximum.");
+	    }
+	    else {
+	    	System.out.println("\n" + N3 + " is maximum.");
+	    }
 	}
 }
